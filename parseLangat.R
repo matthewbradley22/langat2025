c8e4337fe3aea9d2a9f3dd78fe4f724eaeaec317
Matthew Bradley <matthewbradley@client169-132.wireless.umu.se>
2025-08-01 16:08:49 +0200

    R script update

--- parseLangat.R
+++ parseLangat.R
@@ -14,14 +14,34 @@ ParseSeuratObj <- lapply(ParseMatricies, FUN = function(x){
                      min.cells = 3, min.features = 200)
 })
 
-#Run through data processing before integration
-ParseSeuratObj <- lapply(ParseSeuratObj, FUN = function(x){
-  dat <- NormalizeData(x)
-  dat <- FindVariableFeatures(dat)
-  dat <- ScaleData(dat)
-  dat <- RunPCA(dat)
-  dat
-})
+toMerge <- c(ParseSeuratObj[[2]], ParseSeuratObj[[3]], ParseSeuratObj[[4]],
+             ParseSeuratObj[[5]], ParseSeuratObj[[6]], ParseSeuratObj[[7]],
+             ParseSeuratObj[[8]])
+#Merge data before integration
+ParseSeuratObj <- merge(ParseSeuratObj[[1]], y = toMerge, 
+                  add.cell.ids = seq(1,8), project = "ParseLangat")
 
-pbmc.big <- merge(pbmc3k, y = c(pbmc4k, pbmc8k), add.cell.ids = c("3K", "4K", "8K"), project = "PBMC15K")
+#Run through data processing and visualization before integration
+ParseSeuratObj <- NormalizeData(ParseSeuratObj)
+ParseSeuratObj <- FindVariableFeatures(ParseSeuratObj)
+ParseSeuratObj <- ScaleData(ParseSeuratObj)
+ParseSeuratObj <- RunPCA(ParseSeuratObj)
+ElbowPlot(ParseSeuratObj, ndims = 40)
+ParseSeuratObj <- FindNeighbors(ParseSeuratObj, dims = 1:30, reduction = "pca")
+ParseSeuratObj <- FindClusters(ParseSeuratObj, resolution = 2, cluster.name = "unintegrated_clusters")  
+ParseSeuratObj <- RunUMAP(ParseSeuratObj, dims = 1:30, reduction = "pca")
+DimPlot(ParseSeuratObj, group.by = 'orig.ident')
 
+#Integrate data
+#Need to choose integration method
+ParseSeuratObj_int <- IntegrateLayers(object = ParseSeuratObj, method = CCAIntegration,
+                                      orig.reduction = "pca", new.reduction = "integrated.cca", verbose = FALSE)
+
+# re-join layers after integration
+ifnb[["RNA"]] <- JoinLayers(ifnb[["RNA"]])
+
+ifnb <- FindNeighbors(ifnb, reduction = "integrated.cca", dims = 1:30)
+ifnb <- FindClusters(ifnb, resolution = 1)
+
+ifnb <- RunUMAP(ifnb, dims = 1:30, reduction = "integrated.cca")
+DimPlot(ifnb, reduction = "umap", group.by = c("stim", "seurat_annotations"))
