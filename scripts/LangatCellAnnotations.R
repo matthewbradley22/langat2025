cb988bf92f815ece0d419b08ad1b250b5dfd211c
Matthew Bradley <matthewbradley@client171-173.wireless.umu.se>
2025-08-08 15:13:51 +0200

    add scripts

---
+++ scripts/LangatCellAnnotations.R
@@ -0,0 +1,129 @@
+#CELL ANNOTATION 
+#Load in libraries
+library(Seurat)
+library(SingleR)
+library(celldex)
+library(pheatmap)
+library(dplyr)
+
+#Load in data
+#ParseSeuratObj_int <- LoadSeuratRds('./ccaIntegratedDat.rds')
+ParseSeuratObj_int <- LoadSeuratRds("./FilteredRpcaIntegratedDat.rds")
+####Annotate cell types ####
+#Will try automatic annotation
+#Mouse rna seq reference
+ref <- fetchReference("mouse_rnaseq", "2024-02-26")
+pred <- SingleR(test=ParseSeuratObj_int[['RNA']]$data, ref=ref, labels=ref$label.main)
+ParseSeuratObj_int$singleR_labels <- pred$labels
+plotScoreHeatmap(pred)
+
+#UMAP with cell assignments
+DimPlot(ParseSeuratObj_int, group.by = 'singleR_labels', reduction = "umap.integrated",
+        label = TRUE)
+clusterAssignments <- table(Assigned=pred$pruned.labels, Cluster= ParseSeuratObj_int$seurat_clusters)
+pheatmap(log2(clusterAssignments+10), color=colorRampPalette(c("white", "blue"))(101))
+
+####Data is loaded in with doublets, so can start here ####
+#Look at doublets in integrated data
+DimPlot(ParseSeuratObj_int, group.by = 'scDblFinderLabel')
+ParseSeuratObj_int <- subset(ParseSeuratObj_int, scDblFinderLabel == 'singlet')
+
+#Plot viral counts vs well/treatment. 
+#removing doublets doesn't get rid of viral contamination in PBS samples
+ggplot(ParseSeuratObj_int[[]], aes(x = orig.ident, y = virusCount, col = Treatment))+
+  geom_point() +
+  scale_x_discrete(labels= wellMap$well)+
+  theme(axis.text.x = element_text(angle = 90))
+
+#Look at neo1 across genotypes
+ParseSeuratObj_int$neo_exp <- ParseSeuratObj_int[['RNA']]$data['Neo1',]
+ggplot(ParseSeuratObj_int[[]], aes(x = Well, y = neo_exp))+
+  geom_point() +
+  theme(axis.text.x = element_text(angle = 90))
+
+
+#Count of virus presence
+ParseSeuratObj_int[[]] %>% mutate(virusPresent = ifelse(virusCount>0, 'yes', 'no')) %>% 
+  group_by(Treatment, virusPresent) %>% dplyr::summarise(count = n()) %>% 
+  ggplot(aes(x = Treatment, y = count, fill = virusPresent))+
+  geom_bar(stat = 'identity', position = 'dodge')
+
+#Plot of virus presence over small amount
+ParseSeuratObj_int[[]] %>% mutate(virusHight = ifelse(virusCount>3, 'yes', 'no')) %>% 
+  group_by(Treatment, virusHight) %>% dplyr::summarise(count = n()) %>% 
+  ggplot(aes(x = Treatment, y = count, fill = virusHight))+
+  geom_bar(stat = 'identity', position = 'dodge')
+
+#Look at virus presence across celltypes
+ParseSeuratObj_int[[]] <- ParseSeuratObj_int[[]] %>% mutate(virusPresence = ifelse(virusCount > 0, 'yes', 'no')) 
+  DimPlot(ParseSeuratObj_int, group.by = 'virusPresence')
+
+ParseSeuratObj_int[[]] %>% group_by(singleR_labels, virusPresence) %>% dplyr::summarise(count = n()) %>% 
+  ggplot(aes(x = singleR_labels, y = count, fill = virusPresence))+
+  geom_bar(stat = 'identity', position = 'dodge')+
+  theme(axis.text.x = element_text(angle = 90))
+
+#Look at cell counts per well
+table(ParseSeuratObj_int$Well) %>% barplot()
+
+#Most pbs cells with virus have very low amount, could just filter by amount
+pbsWithVirus <- subset(ParseSeuratObj_int, Treatment == 'PBS' & virusCountPAdj > 0)
+summary(pbsWithVirus$virusCountPAdj)
+hist(pbsWithVirus$virusCountPAdj)
+
+#### Manual annotatTimepoint#### Manual annotation ####
+
+#Paper has some canonical markers for cell types
+#https://umu.diva-portal.org/smash/get/diva2:1897514/FULLTEXT01.pdf
+
+#This paper https://link.springer.com/article/10.1007/s10571-021-01159-3
+#Heterogeneity and Molecular Markers for CNS Glial Cells Revealed by Single-Cell Transcriptomics also 
+#has lists of markers
+
+#Some neuron markers
+FeaturePlot(ParseSeuratObj_int, 'Snap25', reduction = 'umap.integrated')
+FeaturePlot(ParseSeuratObj_int, 'Pcp2', reduction = 'umap.integrated')
+FeaturePlot(ParseSeuratObj_int, 'Rbfox3', reduction = 'umap.integrated')
+FeaturePlot(ParseSeuratObj_int, 'Dpp10', reduction = 'umap.integrated')
+FeaturePlot(ParseSeuratObj_int, 'Syt1', reduction = 'umap.integrated')
+
+
+#Endothelial cells
+FeaturePlot(ParseSeuratObj_int, 'Flt1', reduction = 'umap.integrated')
+
+#Astrocytes
+FeaturePlot(ParseSeuratObj_int, 'Gfap', reduction = 'umap.integrated')
+FeaturePlot(ParseSeuratObj_int, 'Aqp4', reduction = 'umap.integrated')
+FeaturePlot(ParseSeuratObj_int, 'Fgfr3', reduction = 'umap.integrated')
+FeaturePlot(ParseSeuratObj_int, 'Aldh1l1', reduction = 'umap.integrated')
+FeaturePlot(ParseSeuratObj_int, 'Slc1a3', reduction = 'umap.integrated')
+
+#Microglia
+FeaturePlot(ParseSeuratObj_int, 'Ctss', reduction = 'umap.integrated')
+FeaturePlot(ParseSeuratObj_int, 'Csf1r', reduction = 'umap.integrated')
+FeaturePlot(ParseSeuratObj_int, 'Cx3cr1', reduction = 'umap.integrated')
+FeaturePlot(ParseSeuratObj_int, 'C1qa', reduction = 'umap.integrated')
+FeaturePlot(ParseSeuratObj_int, 'Tmem119', reduction = 'umap.integrated')
+FeaturePlot(ParseSeuratObj_int, 'P2ry12', reduction = 'umap.integrated')
+
+#Oligo
+FeaturePlot(ParseSeuratObj_int, 'Mag',reduction = 'umap.integrated')
+FeaturePlot(ParseSeuratObj_int, 'Mog', reduction = 'umap.integrated')
+
+#OPC
+FeaturePlot(ParseSeuratObj_int, 'Pdgfra', reduction = 'umap.integrated')
+FeaturePlot(ParseSeuratObj_int, 'Cspg4', reduction = 'umap.integrated')
+
+#Macrophage markers
+FeaturePlot(ParseSeuratObj_int, 'Ptprc', reduction = 'umap.integrated')
+FeaturePlot(ParseSeuratObj_int, 'Ccr2', reduction = 'umap.integrated')
+
+
+#T cells
+FeaturePlot(ParseSeuratObj_int, 'Cd3g', reduction = 'umap.integrated')
+
+DimPlot(ParseSeuratObj_int, label = TRUE)
+
+
+
+
---
+++ scripts/parseLangatFiltered.R
@@ -0,0 +1,166 @@
+#Very few samples in wells 16, 17, 18
+#Load in libraries
+library(Seurat)
+library(dplyr)
+library(scDblFinder)
+library(readr)
+library(ggplot2)
+library(gridExtra)
+
+#List out our data files, and read them into R
+parseOutput <- list.files("./FilteredParseOutput/")
+
+ParseMatricies <- lapply(parseOutput, FUN = function(x){
+  ReadParseBio(paste0("./FilteredParseOutput/", x))
+})
+
+#Convert data to Seurat objects  
+ParseSeuratObj <- lapply(ParseMatricies, FUN = function(x){
+  CreateSeuratObject(counts = x, project = "Langat")
+})
+
+#Can also combine this data using split-pipe combined, instead of doing it here
+toMerge <- c(ParseSeuratObj[[2]], ParseSeuratObj[[3]], ParseSeuratObj[[4]],
+             ParseSeuratObj[[5]], ParseSeuratObj[[6]], ParseSeuratObj[[7]],
+             ParseSeuratObj[[8]])
+
+#Merge data before integration
+ParseSeuratObj <- merge(ParseSeuratObj[[1]], y = toMerge, 
+                        add.cell.ids = paste0('seuObj', seq(1,8)), project = "ParseLangat")
+
+
+#Use Parse barcoding plate to label cells
+#Going to try just left joining and seeing if virus expression matches up
+ParseBarcodePlate <- read_csv("ParseBarcodePlate.csv")
+ParseBarcodePlate$orig.ident <- c(paste0('0', as.character(seq(1:9))), seq(10,48))
+ParseSeuratObj[[]] <- left_join(ParseSeuratObj[[]], ParseBarcodePlate, by = 'orig.ident')
+
+#Label sublibraries 
+ParseSeuratObj$subLib <- substr(colnames(ParseSeuratObj), start = 1, stop = 7)
+
+#Assign viral reads from labels
+#List files with viral reads
+#importing both undajusted and partially adjusted (after fragmentation) viral counts
+virusCountFiles <- list.files('./viralReadCountsUnadjusted/')
+virusCountFilesP <- list.files('./viralReadCountsPartiallyAdjusted/')
+
+virusCountFiles <- virusCountFiles[grep('P36207', virusCountFiles)]
+
+viralCountsUnadjusted <- lapply(virusCountFiles, FUN = function(x){
+  subLibViralCounts <- read_table(paste0("viralReadCountsUnadjusted/", x), 
+                                  col_names = FALSE)
+  subLibViralCounts
+})
+
+viralCountsPartAdjusted <- lapply(virusCountFilesP, FUN = function(x){
+  subLibViralCounts <- read_table(paste0("viralReadCountsPartiallyAdjusted/", x), 
+                                  col_names = FALSE)
+  subLibViralCounts
+})
+
+#For each sublibrary create a df with counts of virus per cell barcode
+#Also adds a sublibrary column for joining purposes afterwards
+for(i in 1:length(viralCountsUnadjusted)){
+  dat = viralCountsUnadjusted[[i]]
+  dat$subLib <- paste0('seuObj', as.character(i))
+  dat$cell <- substr(dat$X2, start = 6, stop = 14)
+  dat <- dat[c(1,3,4)]
+  colnames(dat)[1] = 'virusCount'
+  viralCountsUnadjusted[[i]] <- dat
+}
+
+for(i in 1:length(viralCountsPartAdjusted)){
+  dat = viralCountsPartAdjusted[[i]]
+  dat$subLib <- paste0('seuObj', as.character(i))
+  dat$cell <- substr(dat$X2, start = 6, stop = 14)
+  dat <- dat[c(1,3,4)]
+  colnames(dat)[1] = 'virusCountPAdj'
+  viralCountsPartAdjusted[[i]] <- dat
+}
+
+viralCountsUnadjusted <- bind_rows(viralCountsUnadjusted)
+viralCountsPartAdjusted <- bind_rows(viralCountsPartAdjusted)
+
+ParseSeuratObj$cell <- substr(colnames(ParseSeuratObj), start = 9, stop = 16)
+
+ParseSeuratObj[[]] <- left_join(ParseSeuratObj[[]], viralCountsUnadjusted, by = c('subLib', 'cell'))
+ParseSeuratObj[[]] <- left_join(ParseSeuratObj[[]], viralCountsPartAdjusted, by = c('subLib', 'cell'))
+
+ParseSeuratObj$virusCount[is.na(ParseSeuratObj$virusCount)] = 0
+ParseSeuratObj$virusCountPAdj[is.na(ParseSeuratObj$virusCountPAdj)] = 0
+
+wellMap <- data.frame(well = c(paste0('A', seq(1,12)), paste0('B', seq(1,12)),
+                               paste0('C', seq(1,12)),   paste0('D', seq(1,12))))
+
+#Plot viral counts vs well/treatment. Looks right
+ggplot(ParseSeuratObj[[]], aes(x = orig.ident, y = virusCountPAdj, col = Treatment))+
+  geom_point() +
+  scale_x_discrete(labels= wellMap$well)+
+  theme(axis.text.x = element_text(angle = 90))
+
+ggplot(ParseSeuratObj[[]], aes(x = virusCount, y = virusCountPAdj))+
+  geom_point()+
+  geom_abline(slope=1)
+
+#Check percent of cells in each group expressing virus
+ParseSeuratObj[[]] %>% mutate(virusPresent = ifelse(virusCount>0, 'yes', 'no')) %>% 
+  group_by(Treatment, virusPresent) %>% dplyr::summarise(count = n()) %>% 
+  ggplot(aes(x = Treatment, y = count, fill = virusPresent))+
+  geom_bar(stat = 'identity', position = 'dodge')
+
+#Label mitochondrial gene expression
+ParseSeuratObj[["percent.mt"]] <- PercentageFeatureSet(ParseSeuratObj, pattern = "^mt-")
+
+
+#Look at data features
+Idents(ParseSeuratObj) <- "all"  #Stops violin plot from grouping by seurat cluster
+VlnPlot(ParseSeuratObj, features = c("nFeature_RNA", "nCount_RNA", "percent.mt"),
+        ncol = 3, pt.size = 0)
+
+#Run through data processing and visualization before integration
+ParseSeuratObj <- NormalizeData(ParseSeuratObj)
+ParseSeuratObj <- FindVariableFeatures(ParseSeuratObj)
+ParseSeuratObj <- ScaleData(ParseSeuratObj)
+ParseSeuratObj <- RunPCA(ParseSeuratObj)
+ElbowPlot(ParseSeuratObj, ndims = 40)
+ParseSeuratObj <- FindNeighbors(ParseSeuratObj, dims = 1:30, reduction = "pca")
+ParseSeuratObj <- FindClusters(ParseSeuratObj, resolution = 2, cluster.name = "unintegrated_clusters")  
+ParseSeuratObj <- RunUMAP(ParseSeuratObj, dims = 1:30, reduction = "pca", reduction.name = "umap.unintegrated")
+DimPlot(ParseSeuratObj)
+
+#Look at potential doublets
+Parse_sce <- ParseSeuratObj %>% JoinLayers() %>%  as.SingleCellExperiment()
+
+#Use sublibraries as samples for scdblfinder
+Parse_sce <- scDblFinder(Parse_sce, dbr.sd=1, samples = 'subLib')
+table(Parse_sce$scDblFinder.class)
+ParseSeuratObj$scDblFinderLabel <- Parse_sce$scDblFinder.class
+DimPlot(ParseSeuratObj, group.by = 'scDblFinderLabel')
+
+#Keep doublets for now, see if they group together
+#ParseSeuratObj <- subset(ParseSeuratObj, scDblFinderLabel == 'singlet')
+
+
+#Integrate data
+#choosing integration method - rpca fastest, cca takes 5+ hours
+ParseSeuratObj_int <- IntegrateLayers(object = ParseSeuratObj, method = RPCAIntegration,
+                                      orig.reduction = "pca",  new.reduction = "integrated.rpca", 
+                                      verbose = TRUE)
+
+#re-join layers after integration
+ParseSeuratObj_int[["RNA"]] <- JoinLayers(ParseSeuratObj_int[["RNA"]])
+
+ParseSeuratObj_int <- FindNeighbors(ParseSeuratObj_int, reduction = "integrated.rpca", dims = 1:30)
+ParseSeuratObj_int <- FindClusters(ParseSeuratObj_int, resolution = 1)
+
+ParseSeuratObj_int <- RunUMAP(ParseSeuratObj_int, dims = 1:30, reduction = "integrated.rpca", 
+                              reduction.name = "umap.integrated")
+
+intUMAP <- DimPlot(ParseSeuratObj_int, reduction = "umap.integrated", group.by = c("seurat_clusters"), label = TRUE)+
+  NoLegend()+
+  ggtitle('Integrated UMAP')
+unIntUmap <- DimPlot(ParseSeuratObj_int, reduction = "umap.unintegrated", group.by = c("seurat_clusters"), label = TRUE)+
+  NoLegend()+
+  ggtitle('Unintegrated UMAP')
+
+grid.arrange(intUMAP, unIntUmap, ncol=2)
