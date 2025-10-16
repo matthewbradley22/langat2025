5eda5632411b0edce0e229581697d264a6d74190
Matthew Bradley <matthewbradley@client168-237.wireless.umu.se>
2025-10-16 14:09:12 +0200

    upate

--- scripts/gal3Project.R
+++ scripts/gal3Project.R
@@ -10,7 +10,28 @@ newCols <-  c(brewer.pal(12, 'Paired'), '#99FFE6', '#8A5730', '#18662E',  '#FF8A
 DimPlot(ParseSeuratObj_int, label = FALSE, group.by = 'manualAnnotation', reduction = 'umap.integrated',
         cols = newCols)
 
+#ReUMAP
 wt_cerebrum <- subset(ParseSeuratObj_int, Treatment %in% c('PBS', 'rLGTV') & Organ == 'Cerebrum')
+
+wt_cerebrum <- prepSeuratObj(wt_cerebrum)
+ElbowPlot(wt_cerebrum, ndims = 40)
+wt_cerebrum <- prepUmapSeuratObj(wt_cerebrum, nDims = 20, reductionName = 'wt.cerebrum.umap')
+
+DimPlot(wt_cerebrum, reduction = 'wt.cerebrum.umap', label = TRUE)
+DimPlot(wt_cerebrum, reduction = 'wt.cerebrum.umap', group.by = 'manualAnnotation', cols = newCols)+
+  ggtitle('WT Cerebrum UMAP')
+
+#Barplot of proportions
+barDat <- wt_cerebrum[[]] %>% dplyr::group_by(Treatment, manualAnnotation) %>% 
+  dplyr::summarise(total = n()) %>% dplyr::mutate(prop = total/sum(total)) %>% 
+  arrange(desc(total)) 
+
+order <- subset(barDat, Treatment == 'PBS')
+barDat$manualAnnotation = factor(barDat$manualAnnotation, levels = order$manualAnnotation)
+barDat %>% ggplot(aes(x = Treatment, y = prop, fill = manualAnnotation))+
+  geom_bar(stat = 'identity', position = 'stack')+
+  scale_fill_manual(values = newCols)
+
 #Immune cell subset
 #Need to finish labelling last mo/microglia cluster
 immune <- subset(wt_cerebrum, manualAnnotation %in% c('Microglia', 'Macrophage/Monocytes', 'B Cells',
@@ -19,7 +40,10 @@ immune <- subset(wt_cerebrum, manualAnnotation %in% c('Microglia', 'Macrophage/M
 #ReUMAP
 immune <- prepSeuratObj(immune)
 ElbowPlot(immune, ndims = 40)
+immune <- prepUmapSeuratObj(immune, nDims = 25, reductionName = 'immune.umap')
 
-immune <- prepUmapSeuratObj(immune, nDims = 30, reductionName = 'immune.umap')
+#Plug granulocytes into allan brain atlas and check
+DimPlot(immune, reduction = 'immune.umap', label = TRUE)
+DimPlot(immune, reduction = 'immune.umap', group.by = 'manualAnnotation')
 
-dimPlot(immune, reduction = 'immune.umap')
+
